79b26536382bf7909524f474d1e0fdad6501a28c
neha9044 <your-email@example.com>
2025-10-17 16:05:17 +0530

    last push

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,12 @@
+# Modified tests/test_app.py
+import sys
+import os
+
+# Get the path to the project root (one level up from 'tests')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Now, import app
 from app import add
+
 def test_add():
     assert add(2, 3) == 5
